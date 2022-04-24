d7f75f36d55ead862dcdd099c9509be741c6ef5f
Ilya Rogovoy <16943792+IlyaER@users.noreply.github.com>
2022-04-24 22:18:27 +0300

    reviewed version

--- homework.py
+++ homework.py
@@ -6,6 +6,9 @@ from http import HTTPStatus
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
+from telegram.error import TelegramError
+
+import settings
 
 load_dotenv()
 
@@ -14,16 +17,8 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 6
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
@@ -41,10 +36,10 @@ def send_message(bot, message):
     logging.info(f"Отправка сообщения в Telegram: {message}")
     try:
         bot.send_message(
-            chat_id=TELEGRAM_CHAT_ID,
+            chat_id='fake',
             text=message,
         )
-    except Exception as error:
+    except TelegramError as error:
         message = f'Сбой отправки сообщения: {error}'
         logging.exception(message)
 
@@ -54,24 +49,36 @@ def get_api_answer(current_timestamp):
     logging.info("Запрос к API")
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    try:
+        homework_statuses = requests.get(
+            settings.ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+    except requests.exceptions.RequestException as error:
+        raise Exception(f"Проблема при подключении: {error}")
     logging.debug(f"Получен ответ от API {homework_statuses}")
     if homework_statuses.status_code != HTTPStatus.OK:
         raise ValueError("Ответ API не 200")
-    return homework_statuses.json()
+    try:
+        hw_statuses = homework_statuses.json()
+    except ValueError as error:
+        raise ValueError(f"Ответ не в том формате {error}")
+    return hw_statuses
 
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
     logging.info("Проверка API на корректность")
     logging.info(response)
-    if isinstance(response, list):
+    if not isinstance(response, dict):
         raise TypeError("API вернул неверный ответ, ответ не есть список!")
+    if 'homeworks' not in response.keys():
+        raise ValueError('Нет ключа homeworks')
     if not isinstance(response.get('homeworks'), list):
         raise TypeError("API вернул неверный ответ")
     if not response.get('homeworks'):
         return []
-
     logging.debug(f"Ответ API корректен: {response}")
     try:
         response.get('homeworks')
@@ -84,11 +91,13 @@ def parse_status(homework):
     """Извлекает из информации о конкретной домашней работе её статус."""
     logging.debug("Извлечение информации из запроса:")
     logging.debug(homework)
+    if ('homework_name' or 'status') not in homework.keys():
+        raise KeyError('Нет нужной информации в запросе')
     homework_name = homework['homework_name']
     homework_status = homework['status']
 
-    verdict = HOMEWORK_STATUSES.get(homework_status)
-    if not verdict:
+    verdict = settings.HOMEWORK_STATUSES.get(homework_status)
+    if verdict is None:
         logging.error("Нет такого статуса")
         raise KeyError("Нет такого статуса")
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -97,33 +106,28 @@ def parse_status(homework):
 def check_tokens():
     """Проверяет доступность необходимых переменных окружения."""
     logging.info("Проверка токенов")
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        logging.info("Токены присутствуют")
-        return True
-    else:
-        logging.critical("Токены отсутствуют. Проверьте наличие файла .env")
-        return False
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
-    logging.info("Запущен бот")
+    if not check_tokens():
+        raise Exception("Токены отсутствуют. Проверьте наличие файла .env")
 
+    logging.info("Запущен бот")
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = 1
 
     hw_status = ''
     error_status = ''
 
-    if not check_tokens():
-        raise Exception("Токены отсутствуют. Проверьте наличие файла .env")
     while True:
         try:
             response = get_api_answer(current_timestamp)
             current_timestamp = response["current_date"]
             check = check_response(response)
             if not check:
-                if hw_status == '':
+                if not hw_status:
                     raise ValueError('Список работ пуст')
                 logging.debug('Новых статусов не появилось')
                 continue
@@ -141,7 +145,7 @@ def main():
                 send_message(bot, message)
             error_status = message
         finally:
-            time.sleep(RETRY_TIME)
+            time.sleep(settings.RETRY_TIME)
 
 
 if __name__ == '__main__':
